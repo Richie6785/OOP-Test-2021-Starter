97bce4e13bb71621fc4a59f0484fe8223d5f3e02
Richie6785 <richardass99@live.co.uk>
2021-04-21 16:50:14 +0100

    Added appropriate constructor and toString method

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -3,4 +3,46 @@ package ie.tudublin;
 public class Note {
     private String note;
     private int duration;
+    private float type;
+
+    public Note()
+    {
+
+    
+    }
+
+    public Note(String note , int duration, float type)
+    {
+    this.note = note;
+    this.duration = duration;
+    this.type = type;
+
+    }
+
+
+    public String getNote() {
+        return note;
+    }
+    public void setNote(String note) {
+        this.note = note;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    public float getType() {
+        return type;
+    }
+
+    public void setType(float type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Note [duration=" + duration + ", note=" + note + ", type=" + type + "]";
+    }
 }
